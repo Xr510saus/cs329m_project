b7211cebd6b6cb5b11ea34907ed67b487f1a29de
Xr510saus <ninjapiemasteris@gmail.com>
2024-12-01 17:27:46 -0800

    Modified test_on_test_dev_set to no longer create intermediary files, and run testing directly without needing to split the input files

--- test_on_test_dev_set.py
+++ test_on_test_dev_set.py
@@ -1,134 +1,71 @@
 import os
-import time
+import torch
 import argparse
 import numpy as np
 import pandas as pd
 import tree_sitter as ts
 import tree_sitter_cpp as tscpp
 
-from main import test
+from main import test, tokenize_batch
 from tree_sitter import Language, Parser
 from notebooks.utils import all_paths_exist
+from model import SideEffectClassificationModel
+from transformers import AutoTokenizer, RobertaConfig
 
 
-CODE_DIR_NAME = 'split_cpps'
-LABEL_DIR_NAME = 'split_labels'
+CODE_DIR_NAME = 'CPP_Files'
+LABEL_DIR_NAME = 'Labels'
 LINE_DATASET = 'line_dataset'
 CUTOFF_DATASET = 'cutoff_dataset'
 AST_DATASET = 'ast_dataset'
 AST_NO_CODE_DATASET = 'ast_no_code_dataset'
-TEST_DATASET = 'test_dataset'
-COL_NAMES = ['line', 'code', 'label']
 
 SET_NAME = 'register_core_types'
 DESIRED_INPUT_TYPE_DATASET = LINE_DATASET
 
 DEFAULT_CONFIG_NAME = 'microsoft/graphcodebert-base'
 DEFAULT_TOKENIZER = DEFAULT_CONFIG_NAME
-DEFAULT_BATCH_SIZE = 16 # empirically found to be most time efficient
+
+BEST_MODEL_WEIGHTS = 'best_model_weights.pth'
+COL_NAMES = ['line', 'label']
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-def create_data_files(input_type: str,
-                      code_dir: str,
-                      label_dir: str,
-                      test_data_file: str,
-                      file_names: list[str],
-                      extension_tracker: dict[str, str],
-                      parser: ts.Parser)->None:
-    container = []
-
-    for name in file_names:
-        cpp_file = f'{code_dir}/{name}.{extension_tracker[name]}'
-        txt_file = f'{label_dir}/{name}.txt'
-        
-        if not all_paths_exist([cpp_file, txt_file]):
-            raise Exception(f'Could not find {cpp_file} and {txt_file}')
-        
-        with (open(cpp_file, 'r') as code, 
-            open(txt_file, 'r') as labels):
-            code_lines = code.read().splitlines()
-            label_lines = labels.readlines()
-            
-            if len(code_lines) != len(label_lines):
-                raise Exception(f'Length mismatch for {name}.')
-            
-            for i in range(len(code_lines)):
-                curr_label = int(label_lines[i])
-                
-                if input_type == LINE_DATASET:
-                    curr_line = f'Line {i+1}: {code_lines[i].strip()}'
-                    stripped_code = [line.strip() for line in code_lines]
-                    curr_code_block = '\t'.join(stripped_code)
-                    
-                elif input_type == CUTOFF_DATASET:
-                    curr_line = 'Side effect free?'
-                    stripped_code = [line.strip() for line in code_lines[:i+1]]
-                    curr_code_block = '\t'.join(stripped_code)
-                    
-                elif input_type == AST_DATASET:
-                    stripped_code = [line.strip() for line in code_lines[:i+1]]
-                    tree = parser.parse(bytes('\n'.join(stripped_code), 
-                                              encoding='utf-8'))
-                    curr_line = str(tree.root_node)
-                    curr_code_block = '\t'.join(stripped_code)
-                    
-                elif input_type == AST_NO_CODE_DATASET:
-                    stripped_code = [line.strip() for line in code_lines[:i+1]]
-                    tree = parser.parse(bytes('\n'.join(stripped_code), 
-                                              encoding='utf-8'))
-                    curr_line = str(tree.root_node)
-                    curr_code_block = 'na'
-                    
-                else:
-                    raise Exception(f'Dataset name "{input_type}" unknown.')
-                
-                write_str = [curr_line, curr_code_block, curr_label]
-                container.append(write_str)
-      
-    container = np.array(container)
+def load_model_weights(config_name: str,
+                       model_weights_path: str)->SideEffectClassificationModel:
     
-    df = pd.DataFrame(container, columns=COL_NAMES)
-    df.to_csv(test_data_file, index=False)
+    config = RobertaConfig.from_pretrained(config_name)
+    model = SideEffectClassificationModel(config).to(device)
+    model.load_state_dict(torch.load(model_weights_path, weights_only=True))
+    
+    return model
 
 
 def main():
     parser = argparse.ArgumentParser()
+    
     parser.add_argument("--test_set_name", default=SET_NAME,
                         type=str, help="Dataset under dev_test_set/")
-    parser.add_argument("--code_dir_name", default=CODE_DIR_NAME,
-                        type=str, help="Dir containaing pre-split code files.")
-    parser.add_argument("--label_dir_name", default=LABEL_DIR_NAME,
-                        type=str, help="Dir containaing pre-split labels.")
     parser.add_argument("--desired_input_type_dataset", 
                         default=DESIRED_INPUT_TYPE_DATASET,
                         type=str, help="Which model to use for testing.")
-    args = parser.parse_args()
-    
-    proj_dir = os.path.dirname(os.path.realpath('__file__'))
-    test_set_dir = f'{proj_dir}/dev_test_set/{args.test_set_name}'
-    code_dir = f'{test_set_dir}/{args.code_dir_name}'
-    label_dir = f'{test_set_dir}/{args.label_dir_name}'
-
-    test_data_file = f'{test_set_dir}/test.csv'
-    saved_model_dir = f'{args.desired_input_type_dataset}_saved_models'
-    
-    parser.add_argument("--test_data_file", default=test_data_file, 
-                        type=str, help="Test data file.")
-    parser.add_argument("--output_dir", default=saved_model_dir, type=str,
-                        help="Where the model checkpoints will be written.")
     parser.add_argument("--config_name", default=DEFAULT_CONFIG_NAME, 
                         type=str, help="Pretrained config name.")
     parser.add_argument("--tokenizer_name", default=DEFAULT_TOKENIZER, 
                         type=str, help="Pretrained tokenizer name.")
-    parser.add_argument("--batch_size", default=DEFAULT_BATCH_SIZE, 
-                        type=int, help="Batch size.")
+    
     args = parser.parse_args()
     
+    test_set_dir = f'dev_test_set/{args.test_set_name}'
+    code_dir = f'{test_set_dir}/{CODE_DIR_NAME}'
+    label_dir = f'{test_set_dir}/{LABEL_DIR_NAME}'
+    
     if not all_paths_exist([code_dir, label_dir]):
-        err = f'Missing "{code_dir}" & "{label_dir}".'
+        err = f'Missing "{code_dir}" or "{label_dir}".'
         raise Exception(err)
 
-    file_names = []
+    file_name = ''
     extension_tracker = dict()
 
     for _, _, files in os.walk(code_dir):
@@ -136,32 +73,114 @@ def main():
             try:
                 if file[-3:] == '.cc':
                     name = file[:-3]
-                    file_names.append(name)
+                    file_name = name
                     extension_tracker[name] = 'cc'
                 elif file[-4:] == '.cpp':
                     name = file[:-4]
-                    file_names.append(name)
+                    file_name = name
                     extension_tracker[name] = 'cpp'
             except:
                 # Exceptions likely occur due to the filename being less than 
                 # 3/4 chars long, so we can skip since they cannot be the code 
                 # files we're looking for.
                 continue
-
+            
+    code_file_name = f'{code_dir}/{file_name}.{extension_tracker[file_name]}'
+    label_file_name = f'{label_dir}/{file_name}.txt'
+    
+    if not all_paths_exist([code_file_name, label_file_name]):
+        err = f'Missing "{code_file_name}" or "{label_file_name}".'
+        raise Exception(err)
+    
+    input_type = args.desired_input_type_dataset
+    model_dir_name = f'{input_type}_saved_models'
+    model_weights_path = f'{model_dir_name}/{BEST_MODEL_WEIGHTS}'
+    model = load_model_weights(args.config_name,
+                                model_weights_path)
+    model.eval()
+    tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_name)
     lang_parser = Parser(Language(tscpp.language()))
     
-    start_time = time.time()
-    create_data_files(args.desired_input_type_dataset,
-                      code_dir,
-                      label_dir,
-                      test_data_file,
-                      file_names,
-                      extension_tracker,
-                      lang_parser)
-    end_time = time.time()
-    print(f'Time to finish creating test.csv: {end_time - start_time} s\n')
-    
-    test(args, test_set_dir)
+    with (open(code_file_name, 'r') as code_file, 
+          open(label_file_name, 'r') as label_file):
+        code = code_file.read().split('\n')
+        labels = label_file.read().split('\n')
+        
+        assert(len(code) == len(labels))
+        
+        tp = 0
+        tn = 0
+        fp = 0
+        fn = 0
+        labeled_segments = []
+        
+        for i in range(len(code)):
+            if input_type == AST_DATASET:
+                stripped_code = [line.strip() for line in code[:i+1]]
+                tree = lang_parser.parse(bytes('\n'.join(stripped_code),
+                                               encoding='utf-8'))
+                curr_line = str(tree.root_node)
+                curr_code = '\t'.join(stripped_code)
+            
+            elif input_type == AST_NO_CODE_DATASET:
+                stripped_code = [line.strip() for line in code[:i+1]]
+                tree = lang_parser.parse(bytes('\n'.join(stripped_code), 
+                                               encoding='utf-8'))
+                curr_line = str(tree.root_node)
+                curr_code = 'na'
+            
+            elif input_type == CUTOFF_DATASET:
+                curr_line = 'Side effect free?'
+                stripped_code = [line.strip() for line in code[:i+1]]
+                curr_code = '\t'.join(stripped_code)
+            
+            elif input_type == LINE_DATASET:
+                curr_line = f'Line {i+1}: {code[i].strip()}'
+                stripped_code = [line.strip() for line in code]
+                curr_code = '\t'.join(stripped_code)
+            
+            else:
+                err = f'Unknown input dataset type: {input_type}'
+                raise Exception(err)
+            
+            (tokenized_batch, 
+            attn_masks) = tokenize_batch(([curr_line], [curr_code]), tokenizer)
+            
+            with torch.no_grad():
+                y_pred = model(tokenized_batch, attn_masks=attn_masks)
+                y_pred_label = y_pred.argmax().item()
+
+            curr_label = int(labels[i])
+            if y_pred_label == 1 and curr_label == 1:
+                tp += 1
+            elif y_pred_label == 1 and curr_label == 0:
+                fp += 1
+            elif y_pred_label == 0 and curr_label == 1 :
+                fn += 1
+            else:
+                tn += 1
+            
+            segment = [code[i], y_pred_label]
+            labeled_segments.append(segment)
+            
+        accuracy = (tp + tn) / (tp + tn + fn + fp)
+        try:
+            precision = tp / (tp + fp)
+        except:
+            precision = np.inf
+        try:
+            recall = tp / (tp + fn)
+        except:
+            recall = np.inf
+        f1 = 2 * precision * recall / (precision + recall)
+        
+        print(f'Accuracy: {accuracy}')
+        print(f'Precision: {precision}')
+        print(f'Recall: {recall}')
+        print(f'F1 Score: {f1}')
+            
+    df = pd.DataFrame(labeled_segments, columns=COL_NAMES)
+    df.to_csv(f'{test_set_dir}/labeled.csv', index=False)
 
 if __name__ == '__main__':
     main()
